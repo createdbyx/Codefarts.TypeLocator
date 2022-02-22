5ebafe68862276ac52dd77f9c882d3b5c029a2a0
Dean <deanlunz@createdbyx.com>
2022-02-22 13:27:47 -0800

    further code reduction/simplification

--- Codefarts.TypeLocatorNET6/TypeLocator.cs
+++ Codefarts.TypeLocatorNET6/TypeLocator.cs
@@ -19,34 +19,26 @@ public class TypeLocator
         try
         {
             // attempt to create from cache first
-            IEnumerable<Type> foundTypes;
+            IEnumerable<Type> domainTypes;
+            IEnumerable<Type> asmFileTypes;
 
             // if not in cache scan for
-            if (this.ScanDomainForType(typeFilter, out foundTypes))
-            {
-                return foundTypes;
-            }
-
-            if (this.SearchAssemblies(typeFilter, assemblyFiles, context, out foundTypes))
-            {
-                return foundTypes;
-            }
+            this.ScanDomainForType(typeFilter, out domainTypes);
+            this.SearchAssemblies(typeFilter, assemblyFiles, context, out asmFileTypes);
+            return domainTypes.Union(asmFileTypes);
         }
         catch (Exception ex)
         {
-            throw new TypeLoadException($"Type '{typeFilter}' could not be found.", ex);
+            throw new TypeLoadException($"Unexpected exception thrown. See inner exception for details.", ex);
         }
-
-        return Enumerable.Empty<Type>();
     }
 
     private bool ScanDomainForType(Func<Type, bool> typeFilter, out IEnumerable<Type> foundTypes)
     {
         // search through all loaded assemblies
         var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-        var filteredAssemblies = assemblies.AsParallel();
 
-        var results = filteredAssemblies.AsParallel().SelectMany(asm => asm.GetTypes().Where(typeFilter));
+        var results = assemblies.AsParallel().SelectMany(asm => asm.GetTypes().Where(typeFilter));
 
         foundTypes = results;
         return results.Any();
@@ -60,15 +52,17 @@ public class TypeLocator
         var assemblyLoadContext = context == null ? AssemblyLoadContext.Default : context;
 #endif
 
-        var results = assemblyFiles.AsParallel().SelectMany(file =>
-        {
+        var results = assemblyFiles == null
+            ? Enumerable.Empty<Type>()
+            : assemblyFiles.AsParallel().SelectMany(file =>
+            {
 #if NETCOREAPP3_1_OR_GREATER
-            var assembly = assemblyLoadContext.LoadFromAssemblyPath(file);
+                var assembly = assemblyLoadContext.LoadFromAssemblyPath(file);
 #else
-            var assembly = Assembly.LoadFrom(file);
+                var assembly = Assembly.LoadFrom(file);
 #endif
-            return assembly.GetTypes().Where(typeFilter);
-        });
+                return assembly.GetTypes().Where(typeFilter);
+            });
 
         foundTypes = results;
         return results.Any();
